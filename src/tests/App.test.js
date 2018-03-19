d464eb9f8053407f257c2241f664e2fb938393b0
Mosef Asad <mosefasad@gmail.com>
2018-03-19 05:22:39 -0400

    adding more tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../components/App';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('Renders without crashing', () => {
+    shallow(<App />);
+  });
 });
